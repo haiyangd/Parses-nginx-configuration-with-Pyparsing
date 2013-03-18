f1b4a6d1e8ca8946e50d2cebf33c7748233d8dda
fatiherikli <fatiherikli@gmail.com>
2013-03-18 21:58:27 +0200

    Fixed whitespace problems

--- nginxparser/parser.py
+++ nginxparser/parser.py
@@ -2,19 +2,31 @@ from pyparsing import *
 
 
 def parse(source):
+    """
+    Parses nginx configuration from string
+    """
+    # constants
     left_bracket = Literal("{").suppress()
     right_bracket = Literal("}").suppress()
     semicolon = Literal(";").suppress()
     space = White().suppress()
-
     key = Word(alphanums + "_")
     value = CharsNotIn("{};,")
 
-    assignment = Group(key + space + value + semicolon)
+    # rules
+    assignment = Group(key
+                       + space
+                       + value
+                       + semicolon)
 
     block = Forward()
 
-    block << key + Optional(value) + left_bracket + ZeroOrMore(
-        assignment | block) + right_bracket
+    block << Group(
+        Group(key + Optional(space + value))
+        + left_bracket
+        + Group(ZeroOrMore(assignment | block))
+        + right_bracket)
 
-    return OneOrMore(Group(block)).parseString(source)
+    script = OneOrMore(block)
+
+    return script.parseString(source).asList()
