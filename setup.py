539c8e76cb963bc8802cf74ffc5254112b496c1e
anatoly techtonik <techtonik@gmail.com>
2015-02-26 09:19:18 +0300

    No need to keep this as package, transforming to a single file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='Nginxparser',
-      version='0.2',
+      version='0.3',
       description='Nginx Parser',
       author='Fatih Erikli',
       author_email='fatiherikli@gmail.com',
       url='https://github.com/fatiherikli/nginxparser',
-      packages=['nginxparser'],
+      py_modules=['nginxparser'],
       install_requires = [
           'pyparsing>=1.5.5'
       ]
